7350b09468f2a20445bc3bb241de79cc028d15ac
Shashi5657 <sangepushashidhar@gmail.com>
2024-09-03 22:17:09 +0530

    js exercises

--- exercises.js
+++ exercises.js
@@ -1,3 +1,6 @@
-alert('Good Morning')
-alert('Shashidhar')
-console.log(10 + 5)
+alert("Good Morning");
+alert("Shashidhar");
+console.log(10 + 5);
+console.log(20 - 5);
+console.log(2 + 2 - 5);
+// document.body.innerHTML
