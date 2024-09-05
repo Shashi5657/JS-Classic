9f8de95c60d60b39aaa60b5b7fdfa64b542ec7ba
Shashi5657 <sangepushashidhar@gmail.com>
2024-09-05 13:56:56 +0530

    js exercises

--- basics.js
+++ basics.js
@@ -1,1 +1,10 @@
-alert('Hello')
+alert("Hello");
+//order of operations
+// multiply & divide first - but these both have same priority
+// add & subtract next - these both have same priority
+// this is called as operator precedence
+// we can use brackets to control this
+// ex: (1+1) * 3
+
+//rounding a decimal number
+console.log(Math.round(3.55));
--- exercises.js
+++ exercises.js
@@ -4,10 +4,3 @@ console.log(10 + 5);
 console.log(20 - 5);
 console.log(2 + 2 - 5);
 // document.body.innerHTML
-
-//order of operations
-// multiply & divide first - but these both have same priority
-// add & subtract next - these both have same priority
-// this is called as operator precedence
-// we can use brackets to control this
-// ex: (1+1) * 3
