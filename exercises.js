9c931755447e808b2bffb5def152c00fefb0f269
Shashi5657 <sangepushashidhar@gmail.com>
2024-09-06 14:05:41 +0530

    js exercises

--- exercises.js
+++ exercises.js
@@ -1,6 +1,12 @@
-alert("Good Morning");
-alert("Shashidhar");
-console.log(10 + 5);
-console.log(20 - 5);
-console.log(2 + 2 - 5);
+// alert("Good Morning");
+// alert("Shashidhar");
+// console.log(10 + 5);
+// console.log(20 - 5);
+// console.log(2 + 2 - 5);
 // document.body.innerHTML
+
+let summation = 10 + 3 * 8 + 5;
+console.log(summation);
+console.log(summation / 3);
+let total = Math.round(1850 + 2 * 750) / 100;
+console.log(total * 0.1);
