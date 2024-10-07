cdb9b4282894b568920cb5df2cda41247248da1d
Shashi5657 <sangepushashidhar@gmail.com>
2024-10-07 22:46:22 +0530

    functions

--- exercises.js
+++ exercises.js
@@ -94,13 +94,38 @@ const convertToFahrenheit = (celcius) => {
 console.log(convertToFahrenheit(41));
 
 const convertLength = (length, from, to) => {
+  if (from !== "km" && from !== "miles" && from !== "ft") {
+    return `Invalid unit: ${from}`;
+  } else if (to !== "km" && to !== "miles" && to !== "ft") {
+    return `Invalid unit: ${to}`;
+  }
+
   if (from === "km" && to === "miles") {
-    return `${length / 1.6} miles`;
+    return `${length / 1.6} ${to}`;
+  } else if (from === "km" && to === "ft") {
+    return `${length * 3281} ${to}`;
   } else if (from === "miles" && to === "km") {
-    return `${length * 1.6} km`;
+    return `${length * 1.6} ${to}`;
+  } else if (from === "miles" && to === "ft") {
+    return `${length * 5280} ${to}`;
+  } else if (from === "ft" && to === "km") {
+    return `${length / 3281} ${to}`;
+  } else if (from === "ft" && to === "miles") {
+    return `${length / 5280} ${to}`;
+
+    // You can also do if (from === 'km' && to === 'km) { ... }
+    // if (from === to) is a trick you can use to save some code.
   } else if (from === to) {
     return `${length} ${to}`;
   }
 };
 
-console.log(convertLength(50, "km", "miles"));
+console.log(convertLength(50, "miles", "km"));
+console.log(convertLength(32, "km", "miles"));
+console.log(convertLength(50, "km", "km"));
+
+console.log(convertLength(5, "miles", "km"));
+console.log(convertLength(5, "miles", "ft"));
+console.log(convertLength(5, "km", "ft"));
+
+console.log(convertLength(5, "lbs", "lbs"));
